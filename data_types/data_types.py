f532884ad2d99b9a5564dc18ae1f696cc15755a5
RajKKapadia <raajforyou@gmail.com>
2023-03-02 14:36:10 +0530

    recorded videos

---
+++ data_types/data_types.py
@@ -0,0 +1,50 @@
+# # Numerical
+# a = 9
+# b = 10.90
+
+# c = int(10.9)
+# d = float(20)
+
+# print(a, b, c, d)
+
+# # Boolean
+# a = True
+# b = False
+
+# print(a, b)
+
+# String
+# a = 'This is a string'
+# print(a)
+# print(type(a))
+# print(len(a))
+# print(a[0:3])
+# print(a[-1])
+
+# Sequences
+# List
+# a = [1, 2, 3, 4, 'String', True, 1, 20.0]
+# print(a)
+# print(len(a))
+# a.append('This is new value')
+# print(a)
+
+# Tuple
+# a = ('Any', 1, 23.56, True)
+# print(a)
+# print(type(a))
+
+# Mapping
+# a = {
+#     'key': 'value',
+#     1: 12
+# }
+# print(a)
+# print(a.items())
+# a['one'] = 'ABCD'
+# print(a)
+
+#Nonetype
+# a = None
+# print(a)
+# print(type(a))
